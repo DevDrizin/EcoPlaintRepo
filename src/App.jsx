387fd313e152912016d8dbba4ed6aefbd6cec6ba
KaykyLuka <kaykynmluka@gmail.com>
2024-08-27 16:07:46 -0300

    Componentes Relatorio

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Componentes/Header';
+import CentralizadorTexto from './Componentes/CentralizadorTexto';
 
 function App() {
   return (
-
+    <div>
     <Header/>
-
- 
+    <CentralizadorTexto/>   
+    </div>
   );
 }
 
---
+++ src/Componentes/CentralizadorTexto/index.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import TextoRelatorio from '../TextoRelatorio'
+
+export default function CentralizadorTexto(){
+return(
+
+        <div className="Central">
+            <TextoRelatorio/>
+
+        </div>
+
+)
+}
